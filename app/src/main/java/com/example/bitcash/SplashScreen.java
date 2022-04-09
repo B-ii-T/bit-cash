4c212de28e42331b9dcbc834bb6fec11d51e4fc0
Your Name <you@example.com>
2022-04-09 06:49:34 +0100

    splash screen commit

---
+++ app/src/main/java/com/example/bitcash/SplashScreen.java
@@ -0,0 +1,32 @@
+package com.example.bitcash;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+@SuppressLint("CustomSplashScreen")
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent splashIntent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(splashIntent);
+                finish();
+            }
+        },1500);
+    }
+}
